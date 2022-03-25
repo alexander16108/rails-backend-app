c4101791ab7f29278a6713cd95176f6a1ecd0534
alexander16108 <alexandermayowa05@gmail.com>
2022-03-25 12:27:30 +0100

    @chaw-bot Create a table for storing your messages and create 5 different greeting mesasages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create!(greeting: 'Hello, Otto!')
+Greeting.create!(greeting: 'Konbawa, Otto!')
+Greeting.create!(greeting: 'Ekaro, Otto!')
+Greeting.create!(greeting: 'Hola, Otto!')
+Greeting.create!(greeting: 'Bwanji, Otto!')
